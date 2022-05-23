e6d24f6ad9a4d4e03e06b8d293c88066bb58026d
NeresMatheus <78449801+NeresMatheus@users.noreply.github.com>
2022-05-22 22:14:55 -0300

    Separando arquivos por tema de estudo

---
+++ POO/main.py
@@ -0,0 +1,5 @@
+from POO.pessoa import Pessoa
+
+if __name__ == '__main__':
+    p1 = Pessoa('Luiz', 29)
+    pq.comer('maçã')
--- POO/pessoa.py
+++ POO/pessoa.py
@@ -6,10 +6,11 @@ class Pessoa:
     def __init__ (self, nome, idade, comendo=False, falando=False):
         self.nome = nome
         self.idade = idade
-        self.comendo = comento
+        self.comendo = comendo
         self.falando = falando
     
-        #É possivel criar vairaveis ou operações dentro do criador e/ou usalos para geral alguma resposta
+        #É possivel criar vairaveis ou operações dentro do criador e/ou usa-los 
+        #Mas assim como em outros casos ela só existe neste escopo. 
         variavel= 'criado'
         print(variavel)
         #Assim é possivel retornar um valor ou estado durante a criação que n esteja necessariamente relaciodado
@@ -22,6 +23,8 @@ class Pessoa:
             
         print(f'{self.nome} está comendo {alimento}.')
         self.comendo = True
+    
+    
         
         
         
