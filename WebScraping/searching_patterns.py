9cc68f1f32083325a46e4a3aef69f91e049e610e
neres <neresmatheus.m@gmail.com>
2023-07-23 13:21:29 -0300

    creating a library to store all researching patterns of the project

---
+++ WebScraping/searching_patterns.py
@@ -0,0 +1,11 @@
+def sec_pattern():
+    search_pattern = []
+
+    search_pattern.append(
+        'Secretaria de Estado do Meio Ambiente e do Desenvolvimento Sustentável')
+    search_pattern.append(
+        'Secretaria de Estado de Meio Ambiente e Desenvolvimento Sustentável')
+    search_pattern.append(
+        'Secretaria de Estado do Meio Ambiente e do desenvolvimento Sustentável')
+
+    return search_pattern
